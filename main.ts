e7ff727c389d8981792ac098d9f560544a51698f
mcantosesp <93263251+mcantosesp@users.noreply.github.com>
2022-01-19 14:11:29 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,28 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            # # # . .
+            # . . . .
+            # # # . .
+            # . . . .
+            # # # . .
+            `)
+        basic.showNumber(receivedNumber)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        # # # . .
+        . # . . .
+        # # # . .
+        . . . . .
+        `)
+    basic.showNumber(tenpInt)
+})
+let tenpInt = 0
+radio.setGroup(5)
+basic.forever(function () {
+    tenpInt = input.temperature()
+    tenpInt = tenpInt - 0
+})
